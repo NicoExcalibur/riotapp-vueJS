dfea62228909a1b6aeb525c854441e490d0e5263
NicoExcalibur <nico.zanz@gmail.com>
2020-12-15 18:09:35 +0100

    :package:Add axios package & :building_construction:add champions' route

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import Home from './components/Home'
 import Leagueoflegends from './components/Leagueoflegends'
 import Legendsofruneterra from './components/Legendsofruneterra'
+import Champions from './components/Champions'
 
 export default [
     {path: '/', component: Home},
     {path: '/leagueoflegends', component: Leagueoflegends},
     {path: '/legendsofruneterra', component: Legendsofruneterra},
+    {path: '/champions', component: Champions},
 
 ]
---
+++ src/components/Champions.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="container">
+        
+        <h1>Bienvenue sur la page des Champions</h1>
+
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    name: 'Champions',
+    data() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
